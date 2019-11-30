5b32f335e5b39dd84ff269ac277443788c98ca65
Stefan Neidig <s.neidig@appcom-interactive.de>
2019-11-30 22:19:08 +0100

    Update production webpack config

--- webpack.production.js
+++ webpack.production.js
@@ -1,74 +1,37 @@
 const path = require('path');
-const webpack = require('webpack');
-const moment = require('moment');
-
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const buildDir = './dist/production';
+
+const buildDir = './dist';
 
 module.exports = {
   mode: 'production',
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-  },
   entry: {
-    app: './src/index.js'
+    EasyProgressbarPlugin: './src/index.js'
   },
   output: {
     path: path.join(process.cwd(), buildDir),
-    filename: 'app.bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      include: path.join(process.cwd(), 'src/'),
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
-        }
-      }
-    }]
+    filename: '[name].js',
+    library: 'EasyProgressbarPlugin',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([{
-      from: path.join(process.cwd(), 'src', 'assets'),
-      to: './assets',
-    }]),
-    new HtmlWebpackPlugin({
-      hash: true,
-      filename: './index.html',
-      templateParameters: {
-        title: 'Ancient Cave',
-        description: `Last update: ${moment().toISOString()}`
+    new UglifyWebpackPlugin({
+      include: /\.min\.js$/,
+      parallel: true,
+      sourceMap: false,
+      uglifyOptions: {
+        compress: true,
+        ie8: false,
+        ecma: 5,
+        output: {
+          comments: false
+        },
+        warnings: false
       },
-      template: 'src/index.ejs'
-    }),
-    new webpack.DefinePlugin({
-      'typeof CANVAS_RENDERER': JSON.stringify(true),
-      'typeof WEBGL_RENDERER': JSON.stringify(true)
-    }),
-    new UglifyWebpackPlugin()
-  ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      cacheGroups: {
-        dependencies: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-          chunks: 'all',
-          name: 'dependencies'
-        }
-      }
-    }
-  },
-  devServer: {
-    contentBase: path.join(process.cwd(), buildDir)
-  }
+      warningsFilter: (src) => false
+    })
+  ]
 };
